39b5aff353949cf1ab72c7fbafd157f0c289aab4
Kurt <kurt@prometheus.local>
2026-02-11 23:02:41 +0000

    feat: Setup GitHub Actions and OIDC

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
